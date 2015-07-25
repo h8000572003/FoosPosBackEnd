490e4fbfa53a8e029f27b216bf3c58e16787c436
andy <wa.addi@gmail.com>
2015-07-25 14:58:53 +0800

    test

--- SpringMVC/src/test/java/com/food/pos/service/GCMServiceImplTest.java
+++ SpringMVC/src/test/java/com/food/pos/service/GCMServiceImplTest.java
@@ -6,17 +6,17 @@ import org.junit.Test;
 import com.food.pos.AbstractTestJob;
 import com.food.pos.domain.GCMDTO;
 
-public class GCMServiceImplTest  {
+public class GCMServiceImplTest extends AbstractTestJob {
 
 	private GCMService mGCMService;
 
 	@Before
 	public void init() {
-//		this.mGCMService = (GCMService) getComent(GCMService.class);
+		this.mGCMService = (GCMService) getComent(GCMService.class);
 	}
 
 	@Test
 	public void testQueryAll() {
-//		this.mGCMService.queryAll(new GCMDTO());
+		this.mGCMService.queryAll(new GCMDTO());
 	}
 }
